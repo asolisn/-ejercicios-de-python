496d8cb822d8665f5899f8688fecbb6ddbea5a9d
asolisn <85768831+asolisn@users.noreply.github.com>
2021-06-22 14:50:31 -0500

    Add files via upload
    Agregue archivos actualizados.

---
+++ ejercicios de python/ejercicio 10.py
@@ -0,0 +1,11 @@
+num=20
+nombre="arianna"
+print(num,type(num))
+print(nombre,type(nombre))
+
+def mensaje(msg):
+    print(msg)
+    
+    
+    mensaje("Mi primer programa en Python")
+    mensaje("Mi segundo programa en Python")
