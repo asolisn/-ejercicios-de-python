abf4b28fb73567977524f9f2efed7140fd939d88
asolisn <85768831+asolisn@users.noreply.github.com>
2021-06-23 14:53:30 -0500

    Add files via upload

---
+++ ejercicio 11.py
@@ -0,0 +1,10 @@
+class Cargo:
+    def __init__(self):
+        self.codigo=0
+        self.descripcion="Sin carga"
+        
+        
+cargo1 = Cargo()
+print(cargo1.codigo,cargo1.descripcion)
+cargo2 = Cargo()
+print(cargo2.codigo,cargo2.descripcion)
